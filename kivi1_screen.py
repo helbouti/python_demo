3df057739105420df86d1078de10eb19749027f1
helbouti <helbouti@gmail.com>
2022-04-20 19:58:47 -0700

    kivy tutorial

--- kivi1_screen.py
+++ kivi1_screen.py
@@ -1,11 +1,14 @@
+from turtle import title
 from kivymd.app import MDApp
 
 from kivymd.uix.screen import MDScreen
+from numpy import tile
 
 
 class DemoApp(MDApp):
     def build(self):
         screen = MDScreen()
+        screen.md_bg_color=(.3,.3,.3,.3)
         return screen
 
 
--- kivi8.py
+++ kivi8.py
@@ -1,38 +1,13 @@
+
+#cant fix error in this code iam not ready yeat
 from kivymd.app import MDApp
 from kivy.lang import Builder
 
 
-helper="""
-ScreenManager:
-    
-    First_screen:
-
-    Second_screen:
- 
-
-<Second_screen>:
-    name:"second_screen"
-    MDLabel:
-        text:"this is second"
-        pos_hint:{"center_y":.5}
-    MDFlatButton:
-        text:"go to first"
-        pos_hint:{"center_y":.7}
-
-<First_screen>:
-        name:"first_screen"
-        MDLabel:
-            text:"this is first"
-            pos_hint:{"center_y":.5}
-
-        MDFlatButton:
-            text:"go to next"
-            pos_hint:{"center_y":.7}
-        
-"""
 
 class DemoApp(MDApp):
     def build(self):
-        return Builder.load_string(helper)
+        screen =Builder.load_file("kv8.kv")
+        return screen
 
 DemoApp().run()
